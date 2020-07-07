d96d36eb49e9c32f075766421f8958231209b875
natzkie2 <benjie_nonato@umindanao.edu.ph>
2020-07-07 08:26:51 +0800

    update 07/07/20 8:26 am laravel:auth

--- resources/views/pizzas/show.blade.php
+++ resources/views/pizzas/show.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.layout')
+@extends('layouts.app')
 
 @section('content')
 <div class="wrapper pizza-details">
@@ -11,6 +11,11 @@
             <li>{{ $topping }}</li>
         @endforeach
     </ul>
+    <form action="/pizzas/{{ $pizza->id }}" method="POST">
+        @csrf
+        @method('DELETE')
+            <button>Complete Order</button>
+    </form>
 </div>
 <a href="/pizzas" class="back"><- Back to all pizzas</a>
 @endsection
